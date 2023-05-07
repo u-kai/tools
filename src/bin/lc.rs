e0152ebdd290f49ed64a89d98b670b8c0c9394df
u-kai <76635578+u-kai@users.noreply.github.com>
2023-05-07 18:24:40 +0900

    add extension

--- src/bin/lc.rs
+++ src/bin/lc.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::{read_dir, read_to_string},
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 use clap::Parser;
@@ -12,27 +13,64 @@ fn main() {
         None => all_file_path("./"),
     };
     let count = target.iter().fold(0, |mut acc, f| {
-        if f.extension().map(|f| f.to_str()) == Some(Some(&cli.extension))
-            && !contain(f, &cli.ignored)
-        {
+        if cli.extension.is(f) && !&cli.ignored.contain(f) {
             acc += read_to_string(f).unwrap().lines().count();
         };
         acc
     });
     println!("line is {}", count);
 }
-fn contain(f: &PathBuf, s: &Vec<String>) -> bool {
-    let filename = f.to_str().unwrap_or_default();
-    s.iter().any(|s| filename.contains(s.as_str()))
-}
 
 #[derive(Parser)]
 struct Cli {
-    extension: String,
+    #[clap(short, long)]
+    extension: Extension,
     #[clap(short, long)]
     target: Option<String>,
     #[clap(short, long, value_delimiter = ',')]
-    ignored: Vec<String>,
+    ignored: IgnorePath,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Extension(String);
+impl Extension {
+    fn new(s: &str) -> Self {
+        let mut chars = s.chars();
+        if let Some(first) = chars.next() {
+            if first == '.' {
+                return Self(chars.collect::<String>());
+            }
+        }
+        Self(s.to_string())
+    }
+    fn is(&self, f: &PathBuf) -> bool {
+        f.extension().map(|f| f.to_str()) == Some(Some(&self.0))
+    }
+}
+impl FromStr for Extension {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::new(s))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct IgnorePath {
+    paths: Vec<String>,
+}
+impl IgnorePath {
+    fn contain(&self, f: &PathBuf) -> bool {
+        let filename = f.to_str().unwrap_or_default();
+        self.paths.iter().any(|s| filename.contains(s.as_str()))
+    }
+}
+impl FromStr for IgnorePath {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            paths: s.split(",").map(|s| s.trim_start().to_string()).collect(),
+        })
+    }
 }
 
 pub fn all_file_path(root_dir_path: impl AsRef<Path>) -> Vec<PathBuf> {
@@ -56,5 +94,42 @@ pub fn all_file_path(root_dir_path: impl AsRef<Path>) -> Vec<PathBuf> {
             println!("{}", e.to_string());
             panic!("not found path = {:?}", root_dir_path.as_ref())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn extensionは文字列から生成できる() {
+        let extension = Extension::from_str("rs");
+        assert_eq!(extension, Ok(Extension("rs".to_string())));
+        let extension = Extension::from_str(".rs");
+        assert_eq!(extension, Ok(Extension("rs".to_string())));
+    }
+    #[test]
+    fn pathから拡張子を判別できる() {
+        let extension = Extension::from_str("rs").unwrap();
+        let path = PathBuf::from("src/main.rs");
+        assert!(extension.is(&path));
+        let path = PathBuf::from("src/main");
+        assert!(!extension.is(&path));
+    }
+    #[test]
+    fn ignore_pathは文字列から生成できる() {
+        let ignore_path = IgnorePath::from_str("target");
+        assert_eq!(
+            ignore_path,
+            Ok(IgnorePath {
+                paths: vec!["target".to_string()]
+            })
+        );
+        let ignore_path = IgnorePath::from_str("target, Cargo");
+        assert_eq!(
+            ignore_path,
+            Ok(IgnorePath {
+                paths: vec!["target".to_string(), "Cargo".to_string()]
+            })
+        );
     }
 }
